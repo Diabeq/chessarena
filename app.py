ba10b99c7eb37199ce698c281089d7e3ea15883b
Diabeq <167358977+Diabeq@users.noreply.github.com>
2025-07-29 22:29:20 +0200

    Update app.py
    from flask import Flask
    
    app = Flask(__name__)
    
    @app.route("/")
    def home():
        return "Działa z Rendera!"
    
    # WAŻNE: NIE dodawaj "if __name__ == '__main__'" na Renderze

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return 'Działa!'
+    return "Działa z Rendera!"
 
-if __name__ == '__main__':
-    app.run()
+# WAŻNE: NIE dodawaj "if __name__ == '__main__'" na Renderze
